f012b03de55162422b9c930c11ea358ccf10dfe5
R4GHAV <67642579+R4GHAV@users.noreply.github.com>
2021-02-05 21:32:09 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class operators {
+
+	public static void main(String[] args) {
+		
+	   int age = 10;
+	   if(age >= 9) {
+		   System.out.println("you can vote");
+	   }
+	   
+	}
+
+}
